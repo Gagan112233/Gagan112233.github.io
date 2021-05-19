bb7505a821c5491852f83dc64bade9d6a713f0a2
Gagan112233 <gaganujjini96@gmail.com>
2021-05-19 08:02:57 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+let hamburgerButton = document.getElementById("hamburger")
+let navList = document.getElementById("nav-list")
+
+function toggleButton(){
+    navList.classList.toggle('show')
+}
+
+hamburgerButton.addEventListener('click', toggleButton)
